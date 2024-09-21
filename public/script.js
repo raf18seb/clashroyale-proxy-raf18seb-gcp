6752e99c3d30ae89c867b28639da159ce8e2f104
unknown <raf18seb@gmail.com>
2024-09-21 22:45:35 +0200

    Test

--- public/script.js
+++ public/script.js
@@ -1,5 +1,22 @@
 console.log('Loaded.');
 
+const clanTag = "YPJ0VRRR";
+
+fetch(`/clan/${clanTag}`)
+    .then(response => response.json())
+    .then(data => {
+        const clanInfo = document.getElementById('clan-info');
+        clanInfo.innerHTML = `
+            <h2>Clan Name: ${data.name}</h2>
+            <p>Clan Level: ${data.clanLevel}</p>
+            <p>Members: ${data.members}</p>
+            <p>Score: ${data.clanScore}</p>
+        `;
+    })
+    .catch(error => {
+        console.error('Error fetching clan data:', error);
+    });
+
 // Funkcja do pobierania danych o Clan Wars (CW)
 function fetchWarlog(clanTag) {
     fetch(`/clan/${clanTag}/warlog`)
